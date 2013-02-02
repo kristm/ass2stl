d6cff992eb3f184a8fec7e1ce5cf7ae4f2bf0c48
krist <krist@hellowala.org>
2013-02-02 09:19:15 +0800

    remove lein template code

--- src/ass2stl/core.clj
+++ src/ass2stl/core.clj
@@ -1,11 +1,6 @@
 (ns ass2stl.core
     (:use [clojure.java.io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn read-lines
     [filename]
     (let [rdr (reader filename)]
@@ -20,4 +15,3 @@
     [& args]
     (doseq [line (read-lines (first args))]
         (println line)))
-;    (println (first args)))
